93b4f0b9c5d3d9bc447c1034eb6535adbb0616c4
mosesxin <mosesxin@qq.com>
2020-01-27 00:33:56 +0800

    optimize HashUtils

--- moses/hash/Hash.py
+++ moses/hash/Hash.py
@@ -1,26 +1,57 @@
+import _hashlib
 import hashlib
 
 
 def md5(path: (str,)):
-    if not isinstance(path, (str,)):
-        raise RuntimeError("path must be type in (str,)")
-
-    hash_md5 = hashlib.md5()
-    with open(path, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-
-    return hash_md5.hexdigest()
+    return _hash(path, hashlib.md5())
 
 
-def md5_until(path: (str,), extent: (int,)):
+def md5_until(path: (str,), extent: (int,) = 4096):
+    return _hash_until(path, hashlib.md5(), extent)
+
+
+def sha1(path: (str,)):
+    return _hash(path, hashlib.sha1())
+
+
+def _hash(path: (str,), hash_object: (_hashlib.HASH,)):
+    return _hash_until(path, hash_object)
+
+
+def _hash_until(path: (str,), hash_object: (_hashlib.HASH,), extent: (int,) = -1):
+    """
+    common hash until
+    :param path:
+    :param hash_object:
+    :param extent: <0: unlimited, 0: empty, >0: limited
+    :return:
+    """
     if not isinstance(path, (str,)):
         raise RuntimeError("path must be type in (str,)")
 
     if not isinstance(extent, (int,)):
         raise RuntimeError("extent must be type in (int,)")
 
-    hash_md5 = hashlib.md5()
+    default_chunk_size = 4096
+    x = {"extent": extent}
+
+    def _next():
+        chunk_t = b""
+        if x["extent"] < 0:
+            chunk_t = f.read(default_chunk_size)
+        elif x["extent"] == 0:
+            pass
+        elif x["extent"] > default_chunk_size:
+            chunk_t = f.read(default_chunk_size)
+            x["extent"] -= default_chunk_size
+        else:
+            chunk_t = f.read(x["extent"])
+            x["extent"] = 0
+
+        return chunk_t
+
     with open(path, "rb") as f:
-        hash_md5.update(f.read(extent))
-        return hash_md5.hexdigest()
+        while chunk := _next():
+            hash_object.update(chunk)
+
+    return hash_object.hexdigest()
